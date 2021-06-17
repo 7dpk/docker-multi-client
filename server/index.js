314a8c956b2e47ccfd200be6a53d0914bd3d5ad4
bhanupratapkumar <bhanujoshila@gmail.com>
2021-06-17 14:49:56 +0000

    removed postgres erro

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ const pgClient = new Pool({
 
 pgClient.on('connect', () => {
   pgClient
-    .query('CREATE TABLE IF NOT EXISTS values (number INT)')
+    .query('CREATE TABLE IF NOT EXISTS val (number INT)')
     .catch((err) => console.log(err));
 });
 
@@ -41,7 +41,8 @@ app.get('/', (req, res) => {
 });
 
 app.get('/values/all', async (req, res) => {
-  const values = await pgClient.query('SELECT * from values');
+
+  const values = await pgClient.query('SELECT * FROM val');
 
   res.send(values.rows);
 });
@@ -61,7 +62,7 @@ app.post('/values', async (req, res) => {
 
   redisClient.hset('values', index, 'Nothing yet!');
   redisPublisher.publish('insert', index);
-  pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
+  pgClient.query('INSERT INTO val(number) VALUES($1)', [index]);
 
   res.send({ working: true });
 });
